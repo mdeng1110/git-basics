489bb17ed8689d95c36200f1fe4c4fa27f199993
mdeng1110 <maydeng1110@yahoo.com>
2026-04-09 22:14:45 -0700

    update python file

--- pythonfile.py
+++ pythonfile.py
@@ -1,6 +1,13 @@
 import getpass
 
+def add(num1, num2):
+    return num1 + num2
+
 def main():
+    num1 = 3
+    num2 = 5
     print(f"Hello {getpass.getuser()}")
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+
 if __name__ == '__main__':
     main()
